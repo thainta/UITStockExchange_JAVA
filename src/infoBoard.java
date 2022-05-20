8f55683303eb3919a63506f58addf1818e7c9892
anthony-le-goldenowl <anthony.le.goldenowl@gmail.com>
2022-05-20 17:37:50 +0700

    com + stock Ãinfo UI

--- src/infoBoard.java
+++
@@ -1,5 +0,0 @@
-import javax.swing.*;
-
-public class infoBoard {
-    private JPanel mainPanel;
-}
--- src/userProfile.java
+++ src/userProfile.java
@@ -21,15 +21,7 @@ public class userProfile extends JDialog{
         super(parent);
         setTitle("User Profile");
         setContentPane(userProfilePanel);
-<<<<<<< HEAD
-<<<<<<< HEAD
         setMinimumSize(new Dimension(600, 650));
-=======
-        setMinimumSize(new Dimension(600, 600));
->>>>>>> c1263f4 (User Profile UI)
-=======
-        setMinimumSize(new Dimension(600, 650));
->>>>>>> dd79294 (fix UI)
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
